fc20ee202a65c27a2df47db01303f0fa912980c3
Vishwas3000 <timeupvishwa@gmail.com>
2024-12-17 20:11:14 +0530

    first commit

--- RndMetal/ViewController.swift
+++ RndMetal/ViewController.swift
@@ -4,16 +4,30 @@
 //
 //  Created by Vishwas Prakash on 10/09/24.
 //
-
 import UIKit
+import Metal
+import MetalKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct Uniforms {
+    var time: Float
 }
 
+class ViewController: UIViewController {
+    private var metalView: MetalSineWaveView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Create an image to render (replace with your actual image)
+        guard let image = UIImage(named: "test11") else {
+            print("Could not create image")
+            return
+        }
+        view.backgroundColor = .cyan
+        // Create Metal view
+        if let metalView = MetalSineWaveView(frame: view.bounds, image: image) {
+            self.metalView = metalView
+            view.addSubview(metalView)
+        }
+    }
+}
